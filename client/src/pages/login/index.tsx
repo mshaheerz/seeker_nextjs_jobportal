3b78e3f208c8b29a443144506309ae67ddf4b726
mshaheerz <mshaheerkp9@gmail.com>
2023-01-20 10:03:42 +0530

    basic setup

---
+++ client/src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import Login from "@/components/Login/Signin"
+
+
+function index() {
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+  return (
+    <Login />
+  )
+}
+
+export default index
---
+++ client/src/pages/login/signup.tsx
@@ -0,0 +1,8 @@
+import SignUp from "@/components/Login/Signup"
+function signup() {
+  return (
+    <SignUp />
+  )
+}
+
+export default signup
